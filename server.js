a64c04c3459a5955c724ec32863adfec2b2a584c
Jienan Zhang <zjn0505@hotmail.com>
2018-01-19 00:19:42 +0800

    Init refreshed server

---
+++ server.js
@@ -0,0 +1,84 @@
+var request = require('request');
+var redis = require('redis'),
+	client = redis.createClient();
+var multer = require('multer');
+var upload = multer();
+var express = require('express'),
+		app = express(),
+		port = 3002;
+const host = "https://api.qwant.com/api/search/images?count=10&offset=1&q={0}&size=small";
+
+
+app.post('/images', upload.array(), function (req, res, next) {
+	console.log(req.body);
+	var images = [];
+	images = images.concat(req.body.image);
+
+	Promise.all(images.map(queryRedis)).then(function(resp) {
+		console.log(resp);
+		var obj = new Object();
+		obj.size = resp.length;
+		obj.data = resp;
+		res.json(obj);
+	}).catch(function(err) {
+		console.log(err);
+		res.sendStatus(500);
+	});
+});
+
+function queryRedis(source) {
+	// return a new promise.
+	return new Promise(function(resolve, reject) {
+		client.get(source, function(error, reply) {
+			if (error) {
+				reject(Error(error));
+			} else {
+				if (reply === null) {
+					// Promise.resolve().
+					const urlQwant = host.format(source);
+					var options = {
+						url: urlQwant,
+						headers: {
+							'User-Agent': 'request'
+						}
+					};
+					request(options, function(error, response, body) {
+						var json = JSON.parse(body);
+						if (json.status == "success" && !(json.data == undefined) && !(json.data.result == undefined)) {
+							var result = json.data.result;
+							if (!(result.items == null) && !(result.items == undefined) && result.items.length > 0) {
+								// res.send(result.items[0].media);
+								var imgUrl = result.items[0].media;
+								console.log(imgUrl);
+								client.sadd("refreshed:sources", source, redis.print);
+								client.set(source, imgUrl, redis.print);
+								resolve(jsonfy(source, imgUrl));
+							}
+						}
+					});
+				} else {
+					resolve(jsonfy(source, reply));
+				}
+			}
+		});
+	});
+}
+
+
+app.listen(port);
+
+
+function jsonfy(source, imgUrl) {
+	var obj = new Object();
+	obj.source = source;
+	obj.imgUrl = imgUrl;
+	return obj
+}
+
+String.prototype.format = function() {
+    var formatted = this;
+    for( var arg in arguments ) {
+        formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+    }
+    return formatted;
+};
