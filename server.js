9ab3bf4484758998b9f338a9799d9ae3cb3df838
Jienan Zhang <zjn0505@hotmail.com>
2018-01-20 22:16:39 +0800

    Improve design

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
     }
 });
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -37,14 +36,10 @@ app.get('/all-images', function(req, res) {
 				var insertHtml="";
 
 				for (var i = 0; i < resp.length; i++) {
-					
-
 					var sourceName = reply[i];
 					var url = resp[i];
 					insertHtml += template.format(sourceName, url, sourceName);
-
 				}
-
 				$("#holder").html(insertHtml);
 				console.log(reply);
 				res.send($.html());
